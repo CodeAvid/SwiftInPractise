bb7680ed19635d401af6bd444b8da63ecf66d68d
Benjamin Akhigbe <51957344+CodeAvid@users.noreply.github.com>
2024-06-19 02:57:31 +0100

    Initial Commit

---
+++ WorkingWithList/WorkingWithListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkingWithListApp.swift
+//  WorkingWithList
+//
+//  Created by Benjamin Akhigbe on 19/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct WorkingWithListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
